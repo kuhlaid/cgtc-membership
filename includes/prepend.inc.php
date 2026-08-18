ecb28eccd049b1a86c86ba35a10725655c610868
kuhlaid <w.patrick.gale@gmail.com>
2026-08-18 15:17:00 -0400

    test

--- includes/prepend.inc.php
+++ includes/prepend.inc.php
@@ -5,7 +5,7 @@
  */
 
 # load any extra packages installed via composer
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__); # used to read $_ENV array values (.env variables)
 $dotenv->load();
 
