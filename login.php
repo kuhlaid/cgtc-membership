9e842b68c73ad44b287068c1881e6e7fdd7c77aa
kuhlaid <w.patrick.gale@gmail.com>
2026-08-20 07:41:55 -0400

    test

--- login.php
+++ login.php
@@ -8,7 +8,6 @@
  */
 // Include prepend.inc to load Qcodo
 require('includes/prepend.inc.php');
-QSessionDB::set('error', 'testing error log');
 define('__SEL_MENU__',__strClub_Home___);
 $joinUrl=$_ENV['JOIN_URL'];
 require(__INCLUDES__ . '/header.inc.php');
